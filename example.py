b24f3cd89acfd035f7d819f242093903e7847029
Daniel Pauw <dpauw@umd.edu>
2023-03-07 16:50:20 -0500

    removed classmate class

--- example.py
+++ example.py
@@ -8,6 +8,3 @@ class Person:
         name(str): The person's name
         connections (set of Person): Other peoplein the class that know this person.
     """
-
-class classmate:
-    """ Docstring """
