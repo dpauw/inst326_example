7e45295050aa196731ca58322dbd22f9fb8af5c4
Daniel Pauw <dpauw@umd.edu>
2023-03-07 16:35:29 -0500

    added a test file to check configuration

---
+++ example.py
@@ -0,0 +1,10 @@
+""" This is a simple example of using Git for INST 326
+"""
+
+class Person:
+    """ A person in the class.
+    
+    Attributes:
+        name(str): the person's name
+        connections (set of Person): other peoplein the class that know this person.
+    """
